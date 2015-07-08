0563b5bbdd0bc29f063b5a39b28427dea67d0430
Richard Wu <contactrichardwu@gmail.com>
2015-07-09 00:33:59 +0800

    check input function try out

--- js/app.js
+++ js/app.js
@@ -1,35 +1,69 @@
 $(document).ready(function(){
   /*total static, data per game, running state of the game*/
 
-  var option1 = document.getElementById('option1');
-  var option2 = document.getElementById('option2');
-  var option3 = document.getElementById('option3');
-  var brandLogo = document.getElementById('brand-Logo');
 
   /*Total Static*/
   var correctIcon = "correct.png";
   var wrongIcon = "wrong.png";
 
-  /*Data per Game*/
-  var gameData = {
-     logo_first:  {logo: "1.png",option1: "Samsung", option2: "IBM", option3: "Intel", answer: 3},
-     logo_second: {logo: "2.png", options1: "Sony", option2: "Nikon", option3: "Canon", answer: 2 }
+  /* Data per Game, including Logo, 3 Options and Correct Answer */
+   gameData = {
+     logo1: {
+       logo: "1.png",
+       options: ["Samsung", "IBM", "Intel"],
+       answer: "3"
+     },
+     logo2: {
+       logo: "2.png",
+       options: ["Sony", "Nikon", "Canon"],
+       answer: "2"
+     },
    };
+   /*States*/
+   var currentLogo = 1;
+   var correctAnswer = 0;
 
-   gameData.displayLogo = function(){
-     brandLogo.src ="assets/" + gameData.logo_first.logo;
-   };
-   gameData.displayLogo();
+   var option1 = document.getElementById('option1');
+   var option2 = document.getElementById('option2');
+   var option3 = document.getElementById('option3');
+   var brandLogo = document.getElementById('brand-Logo');
 
-   gameData.displayOptions = function(){
-     option1.innerHTML = gameData.logo_first.option1;
-     option2.innerHTML = gameData.logo_first.option2;
-     option3.innerHTML = gameData.logo_first.option3;
-   };
-   gameData.displayOptions();
+   var allOptions = document.getElementsByName('rb');
 
-   gameData.verify = function(){
 
-   };
+     var currentQuestion = gameData["logo"+ currentLogo];
+     console.log(currentQuestion);
+
+     function checkAnswer(){
+       var userPick;
+       var answer = currentQuestion.answer;
+       console.log(answer);
+
+       for(var i = 0; i < allOptions.length; i++){
+         console.log(allOptions[i]);
+       }
+     }
+
+     checkAnswer();
+
+
+     function displayLogo(){
+       brandLogo.src ="assets/" + gameData.logo1.logo;
+     }
+     displayLogo();
+
+     function displayOptions(){
+       option1.innerHTML = gameData.logo1.options[0];
+       option2.innerHTML = gameData.logo1.options[1];
+       option3.innerHTML = gameData.logo1.options[2];
+     }
+     displayOptions();
+
+     document.addEventListener('click', function(){
+
+
+     });
+
+
 
 });
